8151ba8cfebc005347072e6edbfc7980428d7d75
AlejoRMinetti <alejorm@gmail.com>
2021-02-19 11:06:59 -0300

    show list name from id in url

--- main/views.py
+++ main/views.py
@@ -1,6 +1,9 @@
 # views.py file
 from django.http import HttpResponse
+from django.shortcuts import render
+from .models import ToDoList, Item
 
 
 def index(request, id):
-    return HttpResponse("<h1>%s</h1>" % id)
+    ls = ToDoList.objects.get(id=id)
+    return HttpResponse("<h1>%s</h1>" % ls.name)
